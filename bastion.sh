001528108d384d20f49cc981bfd247548626992b
E.Gopi <egopimani1998@gmail.com>
2025-03-10 23:44:35 +0530

    updated

--- bastion.sh
+++ bastion.sh
@@ -46,7 +46,6 @@ VALIDATE $? "kubectl installation"
 # eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-eksctl version
 VALIDATE $? "eksctl installation"
 
 # Helm
