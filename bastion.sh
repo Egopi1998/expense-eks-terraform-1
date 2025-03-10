6afff92a4cdb41bd8a31953eac7886cebc32193d
E.Gopi <egopimani1998@gmail.com>
2025-03-10 23:51:58 +0530

    updated

--- bastion.sh
+++ bastion.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+
 USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
+TIMESTAMP=$(date +%F-%H-%M%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
@@ -19,48 +20,40 @@ VALIDATE(){
     fi
 }
 
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
-else
-    echo "You are super user."
-fi
-
 # docker
-yum install -y yum-utils
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
-systemctl enable docker
-usermod -aG docker ec2-user
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
 VALIDATE $? "Docker installation"
 
+sudo dnf install mysql -y # as we are using RDS it is fully managed by AWS so no SSH to access from bastion we need mysql on bastion
 
 # kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mv kubectl /usr/local/bin/kubectl
+sudo mv kubectl /usr/local/bin/kubectl
 VALIDATE $? "kubectl installation"
 
 # eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
 VALIDATE $? "eksctl installation"
 
-# Helm
+#helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
+chmod 777 get_helm.sh
 ./get_helm.sh
 VALIDATE $? "helm installation"
 
-dnf install mysql -y
-VALIDATE $? "MySQL installation"
 
-#kubens
-git clone https://github.com/ahmetb/kubectx /opt/kubectx
-ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-VALIDATE $? "kubens installation"
+#Nodejs
+sudo dnf module disable nodejs -y
+sudo dnf module enable nodejs:20 -y
+sudo dnf install nodejs -y
+sudo yum install zip -y
 
-#java for jenkins 
-yum install fontconfig java-17-openjdk -y
+# To configure the aws eks cluster
